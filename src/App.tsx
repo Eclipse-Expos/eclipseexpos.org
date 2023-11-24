9dfc56a9ddb4e8ac1794f3e6185aeba7dba5ecfd
Jeffrey Zang <jeffrey-zang@Jerfara-Macbook.local>
2023-11-23 22:14:34 -0500

    feat: epic eclipse animation

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,13 @@ const App = () => {
     randomString = (length:number) => Array.from(Array(length)).map(randomChar).join("");
 
   const handleOnMove = (e:any) => {
-    const rect = card.current?.getBoundingClientRect()
-    let x = e.clientX - (rect?.left || 0)
-    let y = e.clientY - (rect?.top || 0)
+    let x = 30 * (e.clientX / window.innerWidth) + 35
+    let y = 30 * (e.clientY / window.innerHeight) + 35
 
-    letters.current?.style.setProperty("--x", `${x}px`);
-    letters.current?.style.setProperty("--y", `${y}px`);
+    console.log(x, y)
+
+    letters.current?.style.setProperty("--x", `${x}%`);
+    letters.current?.style.setProperty("--y", `${y}%`);
 
     setLetterValue(randomString(10000))
   }
@@ -26,12 +27,12 @@ const App = () => {
   return (
     <div className='grid place-items-center h-screen'>
       <div className='text-center text-neutral-100 z-10 pointer-events-none'>
-        <h1 className='text-3xl font-semibold text-neutral-100'>
+        {/* <h1 className='text-3xl font-semibold text-neutral-200'>
           Eclipse2024
         </h1>
-        <p className='text-neutral-500 mt-4 leading-loose max-w-[40%] mx-auto'>
-          We're hosting the largest youth-led conference the world has ever seen. Join us in Waterloo, late 2024. <span className='font-bold'>Let's break some records</span>. 
-        </p>
+        <p className='text-neutral-400 mt-4 leading-loose max-w-[40%] mx-auto'>
+          We're hosting the largest youth-led conference the world has ever seen. Join us in Waterloo, late 2024. <br/><span className='font-bold'>Let's break some records</span>. 
+        </p> */}
       </div>
       <div className="card-track" onMouseMove={e => handleOnMove(e)}>
         <div className="card-wrapper">
