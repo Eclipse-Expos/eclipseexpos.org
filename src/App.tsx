b9b24190088f3148c8c32e591d3c229e6874ec42
Jeffrey Zang <jeffrey-zang@Jerfara-Macbook.local>
2023-11-25 16:01:00 -0500

    feat: star background

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,10 @@
-import { useRef } from 'react';
-import logo from './assets/logo-white.png'
+import Landing from "./sections/Landing/Landing"
 
 const App = () => {
-
-  const card = useRef<HTMLDivElement>(null);
-  const mouse = useRef<HTMLDivElement>(null);
-
-  const handleOnMove = (e:any) => {
-    let x = 40 * (e.clientX / window.innerWidth) + 30
-    let y = 40 * (e.clientY / window.innerHeight) + 30
-    
-    mouse.current?.animate({
-      "--x": `${x}%`,
-      "--y": `${y}%`
-    }, {
-      duration: 500,
-      fill: "forwards",
-      easing: "ease",
-    });
-  }
-
-  const handleOnEnter = () => {
-    console.log('YEET')
-    mouse.current?.animate({
-      "--x": `58%`,
-      "--y": `37%`
-    }, {
-      duration: 500,
-      fill: "forwards",
-      easing: "ease",
-    });
-  }
-
   return (
-    <div className='grid place-items-center h-screen'>
-      <div className='text-center text-neutral-100 z-20 pointer-events-none'>
-        {/* <img src={logo} alt='Logo' className='w-1/5 m-auto opacity-75 logo'></img> */}
-        <h1 className='text-3xl font-black text-neutral-200'>
-          asdfds
-        </h1>
-        <p className='text-neutral-500 mt-4 leading-loose max-w-[40%] mx-auto'>
-          We're hosting the largest youth-led conference the world has ever seen. Join us in Waterloo, late 2024. <br/><span className='font-bold'>Let's break some records</span>. 
-        </p>
-      </div>
-      <div className="card-track" onMouseMove={e => handleOnMove(e)}>
-        <div className="card-wrapper">
-          <div className="card" ref={card}>
-            <div className="card-gradient"></div>
-            <div className="card-mouse" ref={mouse}></div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <div className="starbg"></div>
+      <Landing />
     </div>
   )
 }
---
+++ src/sections/Landing/Landing.tsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from 'react';
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  const card = useRef<HTMLDivElement>(null);
+  const mouse = useRef<HTMLDivElement>(null);
+
+  const [hovered, setHovered] = useState<boolean>(false);
+
+  const handleOnMove = (e:React.MouseEvent<HTMLDivElement>) => {
+    let x = 30 * (e.clientX / window.innerWidth) + 35
+    let y = 30 * (e.clientY / window.innerHeight) + 35
+    
+    mouse.current?.animate({
+      "--x": `${x}%`,
+      "--y": `${y}%`
+    }, {
+      duration: 300,
+      fill: "forwards",
+      easing: "ease",
+    });
+  }
+
+  const handleOnEnter = () => {
+    console.log('YEET')
+    mouse.current?.animate({
+      "--x": `58%`,
+      "--y": `37%`
+    }, {
+      duration: 500,
+      fill: "forwards",
+      easing: "ease-in",
+    });
+  }
+
+  return (
+    <div className='grid place-items-center h-screen bg-transparent' onMouseMove={e => handleOnMove(e)}>
+      <div className='text-center text-neutral-100 z-10 w-[25rem] rounded-full bg-black aspect-square grid place-items-center'>
+        <div className='p-4'>
+          <h1 className='text-3xl font-black text-neutral-200 block' 
+            onMouseEnter={() => console.log('sadofjas')} 
+          >
+            Eclipse2024
+          </h1>
+          <p className='text-neutral-500 mt-4 leading-loose mx-auto'>
+            We're hosting the largest youth-led conference the world has ever seen. Join us in Waterloo, late 2024. <br/><span className='font-bold'>Let's break some records</span>. 
+          </p>
+        </div>
+      </div>
+      <div className={styles.track}>
+        <div className={styles.wrapper} ref={card}>
+          <div className={styles.gradient}></div>
+          <div className={styles.mouse} ref={mouse}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
