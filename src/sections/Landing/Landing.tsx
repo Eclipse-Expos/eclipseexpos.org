9fc58f290258127f5180a4c0333aa3ac0863dea4
jeffrey-zang <jeffrey.zang.07@gmail.com>
2023-11-27 11:46:36 -0500

    fix: line height

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -28,15 +28,18 @@ const Landing = () => {
 
   return (
     <div className='grid place-items-center h-[100svh] bg-transparent' onMouseMove={e => handleOnMove(e)}>
-      <div className='text-center text-neutral-100 z-10 w-[18rem] md:w-[26rem] rounded-full bg-black aspect-square grid place-items-center'>
+      <div className='text-center text-neutral-100 z-10 w-[18rem] md:w-[27rem] rounded-full bg-black aspect-square grid place-items-center'>
         <div className='p-4'>
           <h1 className='text-2xl md:text-3xl font-black text-neutral-200 block' 
             onMouseEnter={() => console.log('sadofjas')} 
           >
             Eclipse2024
           </h1>
-          <p className='text-sm md:text-base text-neutral-500 mt-4 leading-loose mx-auto max-w-[80%]'>
-            The largest youth-led conference the world has ever seen.<br/><span className='font-bold'>Let's break some records</span>. 
+          <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
+            The largest youth-led conference the world has ever seen.
+          </p>
+          <p className='text-sm md:text-base font-bold text-neutral-500 mt-4'>
+            Let's break some records.
           </p>
         </div>
       </div>
