2de58e4db93367d50c0e7fa0e1c4afe049f3a594
Jeffrey Zang <jeffrey.zang.07@gmail.com>
2023-11-26 21:37:29 -0500

    fix: eclipse resizing

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -5,9 +5,14 @@ const Landing = () => {
   const card = useRef<HTMLDivElement>(null);
   const mouse = useRef<HTMLDivElement>(null);
 
-  // const [hovered, setHovered] = useState<boolean>(false);
-
   const handleOnMove = (e:React.MouseEvent<HTMLDivElement>) => {
+
+    const isMobile = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    if (isMobile) {
+      return;
+    }
+
     let x = 30 * (e.clientX / window.innerWidth) + 35
     let y = 30 * (e.clientY / window.innerHeight) + 35
     
@@ -21,21 +26,9 @@ const Landing = () => {
     });
   }
 
-  // const handleOnEnter = () => {
-  //   console.log('YEET')
-  //   mouse.current?.animate({
-  //     "--x": `58%`,
-  //     "--y": `37%`
-  //   }, {
-  //     duration: 500,
-  //     fill: "forwards",
-  //     easing: "ease-in",
-  //   });
-  // }
-
   return (
     <div className='grid place-items-center h-screen bg-transparent' onMouseMove={e => handleOnMove(e)}>
-      <div className='text-center text-neutral-100 z-10 w-[26rem] rounded-full bg-black aspect-square grid place-items-center'>
+      <div className='text-center text-neutral-100 z-10 w-[18rem] md:w-[26rem] rounded-full bg-black aspect-square grid place-items-center'>
         <div className='p-4'>
           <h1 className='text-3xl font-black text-neutral-200 block' 
             onMouseEnter={() => console.log('sadofjas')} 
