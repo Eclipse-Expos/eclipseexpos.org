e2f967ae55c23cd6cafc62eba91473d763d3c57d
jeffrey-zang <jeffrey.zang.07@gmail.com>
2023-11-29 10:36:39 -0500

    feat: static and animate states for eclipse

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import Landing from "./components/Eclipse/Eclipse"
+import Landing from "./pages/Landing"
 import { Routes, Route } from 'react-router-dom'
+import Eclipse from "./components/Eclipse/Eclipse"
 
 const App = () => {
   return (
     <div>
       <div className='starbg'></div>
       <Routes>
-        <Route path='/' element={
-          <Landing type='default' children={
-            <div className='p-4'>
-              <h1 className='text-2xl md:text-[2rem] font-black text-neutral-200 block'>
-                Eclipse2024
-              </h1>
-              <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
-                The largest youth-led conference the world has ever seen.
-              </p>
-              <p className='text-sm md:text-base font-bold text-neutral-500 mt-4'>
-                Let's break some records.
-              </p>
-            </div>
-          }/>      
-        }/>
+        <Route path='/' element={<Landing />} />
+        <Route path='/static' element={<Eclipse type='static' />} />
+        <Route path='/animate' element={<Eclipse type='animate' />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,21 @@
+import Eclipse from '../components/Eclipse/Eclipse'
+
+const Landing = () => {
+  return (
+    <Eclipse type='default' children={
+      <div className='p-4'>
+        <h1 className='text-2xl md:text-[2rem] font-black text-neutral-200 block'>
+          Eclipse2024
+        </h1>
+        <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
+          The largest youth-led conference the world has ever seen.
+        </p>
+        <p className='text-sm md:text-base font-bold text-neutral-500 mt-4'>
+          Let's break some records.
+        </p>
+      </div>
+    }/>
+  )
+}
+
+export default Landing
