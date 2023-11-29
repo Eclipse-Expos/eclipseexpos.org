dab553f75264cb3f212b6c8199fbbde9789b0190
jeffrey-zang <jeffrey.zang.07@gmail.com>
2023-11-29 09:14:24 -0500

    deps: react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import Landing from "./components/Eclipse/Eclipse"
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <div className='starbg'></div>
-      <Landing type='default' children={
-        <div className='p-4'>
-          <h1 className='text-2xl md:text-3xl font-black text-neutral-200 block' 
-            onMouseEnter={() => console.log('sadofjas')} 
-          >
-            Eclipse2024
-          </h1>
-          <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
-            The largest youth-led conference the world has ever seen.
-          </p>
-          <p className='text-sm md:text-base font-bold text-neutral-500 mt-4'>
-            Let's break some records.
-          </p>
-        </div>
-      }/>
+      <Routes>
+        <Route path='/' element={
+          <Landing type='default' children={
+            <div className='p-4'>
+              <h1 className='text-2xl md:text-[2rem] font-black text-neutral-200 block'>
+                Eclipse2024
+              </h1>
+              <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
+                The largest youth-led conference the world has ever seen.
+              </p>
+              <p className='text-sm md:text-base font-bold text-neutral-500 mt-4'>
+                Let's break some records.
+              </p>
+            </div>
+          }/>      
+        }/>
+      </Routes>
     </div>
   )
 }
