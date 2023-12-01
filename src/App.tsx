983dddb3dfab6ac38877a3553c7dd635afd4a269
jeffrey-zang <jeffrey.zang.07@gmail.com>
2023-12-01 16:36:53 -0500

    feat: team page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import Landing from "./pages/Landing"
 import { Routes, Route } from 'react-router-dom'
 import Eclipse from "./components/Eclipse/Eclipse"
 
+import hooman from './assets/hooman.webp'
+import jeffrey from './assets/jeffrey.jpeg'
+import moulik from './assets/moulik.jpeg'
+
 const App = () => {
   return (
     <div>
@@ -11,6 +15,26 @@ const App = () => {
         <Route path='/static' element={<Eclipse type='static' />} />
         <Route path='/animate' element={<Eclipse type='animate' />} />
       </Routes>
+      <div className='mx-auto mt-16 max-w-[60rem]'>
+        <h1 className='text-center font-black text-2xl text-neutral-200'>Our Team</h1>
+        <div className='mx-auto flex flex-col md:flex-row text-neutral-200 mt-8 text-center'>
+          <div className='w-1/3 text-center flex flex-col items-center justify-center gap-4'>
+            <img src={hooman} alt='hooman' className='rounded-full aspect-square w-3/4' />
+            <h2 className='text-neutral-200 text-center text-lg mt-2'>Hooman Reza Nezhad</h2>
+            <p className='text-neutral-400'>President</p>
+          </div>
+          <div className='w-1/3 text-center flex flex-col items-center justify-center gap-4'>
+            <img src={jeffrey} alt='jeffrey' className='rounded-full aspect-square w-3/4' />
+            <h2 className='text-neutral-200 text-center text-lg'>Jeffrey Zang</h2>
+            <p className='text-neutral-400'>Vice President</p>
+          </div>
+          <div className='w-1/3 text-center flex flex-col items-center justify-center gap-4'>
+            <img src={moulik} alt='moulik' className='rounded-full aspect-square w-3/4' />
+            <h2 className='text-neutral-200 text-center text-lg'>Moulik Budhiraja</h2>
+            <p className='text-neutral-400'>Chief Technology Officer</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
