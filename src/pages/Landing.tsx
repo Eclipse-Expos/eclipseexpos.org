91050c0ec7a84e5b26cd0b45fae610cf49fd7419
jeffrey-zang <jeffrey.zang.07@gmail.com>
2023-12-05 08:10:18 -0500

    refac: update heading font

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <Eclipse type='default' children={
       <div className='p-4'>
-        <h1 className='text-2xl md:text-[2rem] font-black text-neutral-200 block'>
+        <h1 className='text-2xl md:text-[2.25rem] text-neutral-200 block'>
           Eclipse2024
         </h1>
         <p className='text-sm md:text-base text-neutral-500 mt-8 mx-auto max-w-[80%]'>
