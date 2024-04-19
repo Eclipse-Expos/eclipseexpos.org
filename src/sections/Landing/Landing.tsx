e10d6dc0032451831a22fcbde05f975762827910
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-04-19 17:18:55 -0400

    change text

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -1,28 +1,28 @@
-import Eclipse from '../../components/Eclipse/Eclipse';
-import { Button } from 'eclipse-components';
-import banner from '../../assets/img/banner.png';
+import Eclipse from "../../components/Eclipse/Eclipse";
+import { Button } from "eclipse-components";
+import banner from "../../assets/img/banner.png";
 
 const Landing = () => {
   return (
-    <div id='landing'>
+    <div id="landing">
       <Eclipse
-        type='default'
+        type="default"
         children={
-          <div className='p-8'>
+          <div className="p-8">
             {/* <h1 className='text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] text-neutral-200 block font-bold text-center'>
               EclipseExpo
             </h1> */}
-            <img src={banner} alt='EclipseExpo' className='w-4/5 mx-auto' />
-            <p className='text-[0.75rem] sm:text-sm text-neutral-500 mt-4 mx-auto hidden md:block'>
+            <img src={banner} alt="EclipseExpo" className="w-4/5 mx-auto" />
+            <p className="text-[0.75rem] sm:text-sm font-bold text-neutral-500 mt-4 mx-auto hidden md:block">
               The world's largest youth-led event.
             </p>
-            <p className='text-[0.75rem] sm:text-sm font-bold text-neutral-500 mt-4'>
-              Let's break some records.
+            <p className="text-[0.75rem] sm:text-sm text-neutral-500 mt-4">
+              Waterloo, October 2024.
             </p>
             <Button
-              className='mx-auto mt-4 bg-black text-neutral-200 hover:bg-neutral-200 hover:text-black'
+              className="mx-auto mt-4 bg-black text-neutral-200 hover:bg-neutral-200 hover:text-black"
               onClick={() => {
-                window.open('https://apply.eclipseexpos.org/');
+                window.open("https://apply.eclipseexpos.org/");
               }}
             >
               Pre-Register
